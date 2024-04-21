e9f91a4499022f6d9fe145c52bafaa5c59982560
Petr Košťál <kostape4@fit.cvut.cz>
2024-04-21 15:54:01 +0200

    Fix position implementation

--- FITexcel/position.cpp
+++ FITexcel/position.cpp
@@ -1,9 +1,127 @@
 #include "position.h"
 
-CPos::CPos ( std::string_view str ) {}
+CPos::CPos ():
+    coo(0, 0),
+    fixed(false, false) {}
 
-CPos & CPos::copy ( const CPos & pos ) const {}
+CPos::CPos ( std::string_view str ):
+    coo(0, 0),
+    fixed(false, false) {
+        
+    if (str.empty())
+        throw std::invalid_argument("Invalid cell identification.");
 
-bool CPos::operator < ( CPos & pos ) const {}
+    auto it = str.begin();
 
-std::ostream & operator << ( std::ostream os, CPos & pos ) {}
+    if (*it == '$') {
+        fixed.first = true;
+        ++it;
+    }
+
+    for (; std::isalpha(*it); ++it) {
+        coo.first = coo.first * 26 + (*it - 'A');
+    }
+
+    if (*it == '$') {
+        fixed.second = true;
+        ++it;
+    } else if (!std::isdigit(*it)) {
+        throw std::invalid_argument("Invalid cell identification.");
+    }
+
+    for (; std::isdigit(*it); ++it) {
+        coo.second = coo.second * 10 + (*it - '0');
+    }
+
+    if (it != str.end()) {
+        throw std::invalid_argument("Invalid cell identification.");
+    }
+
+}
+
+CPos::CPos ( std::pair<int, int> coo, std::pair<int, int> fixed ):
+    coo(coo),
+    fixed(fixed) {}
+
+CPos CPos::copy ( const std::pair<int, int> & delta ) const { 
+    CPos res( *this );
+
+    if ( !fixed.first )                        
+        res.coo.first += delta.first;
+    if ( !fixed.second )
+        res.coo.second += delta.second;
+
+    if ( res.coo.first < 0 || res.coo.second < 0 )
+        throw std::invalid_argument("Invalid cell identification.");
+
+    return res;
+}
+
+CPos & CPos::operator = ( const CPos & arg ) {
+    if (*this == arg)
+        return *this;
+    
+    this->coo = arg.coo;
+    this->fixed = arg.fixed;
+    return *this;
+}
+
+std::pair<int, int> CPos::operator - ( const CPos & arg) {
+    return std::make_pair( this->coo.first - arg.coo.first, this->coo.second - arg.coo.second);
+}
+
+bool CPos::operator < ( const CPos & arg ) const {
+    if ( this->coo.first != arg.coo.first ) 
+        return this->coo.first < arg.coo.first;
+    else
+        return this->coo.second < arg.coo.second;
+}
+
+bool CPos::operator == ( const CPos & arg ) const {
+    return this->coo.first == arg.coo.first && this->coo.second == arg.coo.second;
+}
+
+void rectIterationSize ( std::vector<CPos> & res, const CPos & start, int w, int h ) {
+    for ( int i = 0; i < w; i++ )
+        for ( int j = 0; j < h; j++ )
+            res . push_back( CPos( std::make_pair(start.coo.first + i, start.coo.second + j),
+                                   std::make_pair(false, false)) );
+
+}
+
+void rectIterationPos ( std::vector<CPos> & res, const CPos & start, const CPos & end ) {
+    int deltaX = abs(start.coo.first - start.coo.first);
+    int deltaY = abs(end.coo.second - end.coo.second);
+
+    return rectIterationSize ( res, start, deltaX, deltaY );
+}
+
+void CPos::getCellName (std::string & name) const {
+    std::string col = "";
+    int colNum = coo.first; 
+
+    if ( fixed.first )
+        name += "$";
+
+    while ( colNum >= 0 ) {            
+        int modulo = colNum % 26;
+        char c = 'A' + modulo;
+        col = c + col;
+        colNum = (colNum - modulo) / 26;
+        if ( !colNum )
+            break;
+    } 
+
+    name += col;
+
+    if ( fixed.second )
+        name += "$";
+    name += std::to_string(coo.second);
+}
+
+std::ostream & operator << ( std::ostream & os, const CPos & pos ) {
+    std::string name = "";
+    pos.getCellName(name);
+    os << name;
+    return os;
+}
--- FITexcel/position.h
+++ FITexcel/position.h
@@ -1,6 +1,7 @@
 #ifndef POS_H
 #define POS_H
 
+#include <cstdlib>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -8,13 +9,20 @@
 class CPos
 {
   public:
+        CPos ();
         CPos ( std::string_view str );
-        CPos & copy ( const CPos & pos ) const;  // reffrence might not work, stack
-        bool operator < ( CPos & pos ) const;
-        friend std::vector<CPos> & rectItertion ( const CPos & start, const CPos & end );
+        CPos ( std::pair<int, int> coo, std::pair<int, int> fixed );
+        CPos copy ( const std::pair<int, int> & delta ) const;  // refference??
+        CPos & operator = ( const CPos & arg );
+        std::pair<int, int> operator - ( const CPos & arg);
+        bool operator < ( const CPos & arg ) const;
+        bool operator == ( const CPos & arg ) const;
+        void getCellName (std::string & name) const;
+        friend void rectIterationSize ( std::vector<CPos> & res, const CPos & start, int w, int h );
+        friend void rectIterationPos ( std::vector<CPos> & res, const CPos & start, const CPos & end );
         friend std::ostream & operator << ( std::ostream & os, const CPos & pos );
   private:
-        std::pair<int, int> pos;
+        std::pair<int, int> coo;
         std::pair<bool, bool> fixed;
 };
 
